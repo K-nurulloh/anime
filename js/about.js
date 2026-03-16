9fdf8b7854e0e7d53e23e97db31561ac1185cbd4
Nurulloh <nurullohkomilov18@gmail.com>
2026-03-16 17:15:00 +0500

    animee

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
-import { ensureSeedData } from './storage.js';
-import { applyTranslations, initLangSwitcher } from './i18n.js';
-import { updateCartBadge } from './ui.js';
+import { ensureSeedData } from '/shop/storage.js';
+import { applyTranslations, initLangSwitcher } from '/shop/i18n.js';
+import { updateCartBadge } from '/shop/ui.js';
 
 ensureSeedData();
 applyTranslations();
