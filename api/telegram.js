2a400648b7b3502d69d98e58f145801bf00aea31
Nurulloh <nurullohkomilov18@gmail.com>
2026-04-01 16:11:01 +0500

    anime

--- api/telegram.js
+++ api/telegram.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
     if (!token || !chatId)
       return res.status(500).json({ ok: false, error: "Missing token or chatId" });
 
+    
     const { text } = req.body || {};
     if (!text)
       return res.status(400).json({ ok: false, error: "text required" });
